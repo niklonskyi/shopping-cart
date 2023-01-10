71477da7773b04c9a4dda3a52fac5e9899da8b0d
niklonskyi <niklonskyi@icloud.com>
2023-01-10 23:45:19 +0200

    Added ShopArticle component

--- src/components/ShopArticle.js
+++ src/components/ShopArticle.js
@@ -1,21 +1,31 @@
 import {Component} from "react";
 import '../styles/shopArticle.sass'
+import { motion } from "framer-motion"
 
-function ShopArticle() {
+function ShopArticle(props) {
+
+    let courtCount = 0;
+
     return (
-        <div className="shop_article">
-            <div className="shop_article_content">
-                <div className="article_image">
-                    <img src="images/richard1.webp" width={'100px'} alt=""/>
-                </div>
-                <div className="article_name">
-                    <h3>Article</h3>
-                </div>
-                <div className="article_text">
-                    <p>Nice mask</p>
+        <motion.div
+        whileHover={{scale: 1.1}}>
+            <div className="shop_article">
+                <div className="shop_article_content">
+                    <div className="article_image">
+                        <img src={props.articleImage} width={'100px'} height={'100px'} alt=""/>
+                    </div>
+                    <div className="article_name">
+                        <h3>{props.articleName}</h3>
+                    </div>
+                    <div className="article_text">
+                        <p>{props.articleText}</p>
+                    </div>
+                    <div className="article_add">
+                        <p><button onClick={props.addToCart}>Add</button></p>
+                    </div>
                 </div>
             </div>
-        </div>
+        </motion.div>
     )
 }
 
