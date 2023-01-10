018fc146ef60ef473d96a386e8cd506c68bd3ab5
niklonskyi <niklonskyi@icloud.com>
2023-01-10 23:45:49 +0200

    Added router

--- src/router/RouteSwitch.js
+++ src/router/RouteSwitch.js
@@ -1,13 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../view/Home";
 import Shop from "../view/Shop";
+import {useState} from "react";
 
 const RouteSwitch = () => {
+
+    const [cartCount, addToCount] = useState(0);
+
+    function updateCount() {
+        addToCount(prevState => {
+            return prevState + 1;
+        })
+    }
+
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/shop" element={<Shop />} />
+                <Route path="/" element={<Home cartCount={cartCount} />} />
+                <Route path="/shop" element={<Shop cartCount={cartCount} addToCart={updateCount}/>} />
             </Routes>
         </BrowserRouter>
     );
