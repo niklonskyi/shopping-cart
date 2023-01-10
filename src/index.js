8319884fc7741b0c556d8ffa84f02f63061bb96e
niklonskyi <niklonskyi@icloud.com>
2023-01-10 23:52:43 +0200

    Added Redux provider

--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import './index.css';
 import './App.sass'
 import RouteSwitch from "./router/RouteSwitch";
 import reportWebVitals from './reportWebVitals';
+import App from "./App";
+
+import store from './store/store'
+import { Provider } from 'react-redux'
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <RouteSwitch />
-  </React.StrictMode>
+    <Provider store={store}>
+            <App/>
+    </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
